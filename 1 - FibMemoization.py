b8e1fae3711e4f0887d88a43cccca0d6f05aad7b
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2022-06-09 23:52:16 +0500

    Update 1 - FibMemoization.py

--- 1 - FibMemoization.py
+++ 1 - FibMemoization.py
@@ -27,15 +27,28 @@ print(fib(30))
 #Ans: MEMOIZATION(store some duplicate sub-problems so that i can just get those results later on.)
 
 
-#Memoization
-#Time complexity()
+# Memoization
+# Time complexity(O(n))
+# =============================================================================
+# 
+# How it is working?
+# To understand this code:
+# - Make fibo tree which will call two recursive calls n-1 and n-2 at each node.
+# - After that from leaf nodes, start to return values and store the results in memo dictionary as well.
+# - check for condition if n in memo retun memo[n] and update your dictionary.
+# - memo is nothing but dictionary which will store "n" as key and fibValue[n] as value. 
+# 
+# 
+# =============================================================================
+
 def fib(n,memo={}):
     if n in memo:
         return memo[n]
     if(n<=2):
         return 1
-    memo[n] = fib(n-1,memo)+fib(n-2,memo)
+    memo[n] = fib(n-1,memo)+fib(n-2,memo)     #why passing memo parameter? so that we can check if condition for updated memo each time.
     return memo[n]
+
 
 print(fib(50))
 
