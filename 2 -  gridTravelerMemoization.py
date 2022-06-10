837fa7781a232984c914c50fd7dbfaf5472b085f
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2022-06-10 17:56:04 +0500

    Update 2 -  gridTravelerMemoization.py

--- 2 -  gridTravelerMemoization.py
+++ 2 -  gridTravelerMemoization.py
@@ -7,10 +7,31 @@
 # =============================================================================================================================
 
 
+#time complexity(2^(m+n))
 def gridTraveler(m,n):
     if(m==0 or n==0):
         return 0
     if(m==1 and n==1):
         return 1
     return gridTraveler(m-1, n) + gridTraveler(m, n-1)
+
+
+# =============================================================================
+# How this above solution is working?
+# Make a tree where each node has two values and two childs,
+# one showing downward grid and one showing right grid until you reach base cases.
+# =============================================================================
+
+
+#memoization to reuse repetitive steps
+def gridTraveler(m,n,memo={}):
+    key = str(m) + "," + str(n)
+    if key in memo:
+        return memo[key]
+    if(m==0 or n==0):
+        return 0
+    if(m==1 and n==1):
+        return 1
+    memo[key] = gridTraveler(m-1, n,memo) + gridTraveler(m, n-1,memo)
+    return memo[key]
 
